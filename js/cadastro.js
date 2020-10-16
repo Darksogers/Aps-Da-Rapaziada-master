75e898e79af818306b7c35d2b6697086373f29f1
Guilherme Rosa <swingthehorizon@hotmail.com>
2020-10-16 19:55:42 -0300

    factory

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,52 +1,33 @@
-function pegarInputs(){
-
-   let name = document.getElementById('txtNome').value;
-   let email = document.getElementById('txtEmail').value;
-   let pass = document.getElementById('txtPass').value;
-   let passCon = document.getElementById('txtPassCon').value;
-
-   function validarSenhas(){
-        if(pass.lenght < 8){
-            alert('Senha muito curta, menor que 8 caracteres');
-            return;
-        }
-       else if(passCon != pass){
-            alert('Senhas não estão iguais, verifique');
-            return;
-        }
-
-        return true;
-   }
-
-   function validarEmail(email){
-        if(  email=="" 
-            || email.indexOf('@')==-1 
-            || email.indexOf('.')==-1 )
-        {
-           alert( "Por favor, informe um E-MAIL válido!" );
-           return false;
-        }
-        return true;
-    }
-
-
-   function gerarJson(){
-
-       let cadastro = {
-
-           "name": name,
-           "email": email,
-           "pass": pass,
-           "passCon": passCon
-        }
-
-       JSON.parse('cadastro')
-    }
-
-    validarEmail(email);
-    validarSenhas();
-    //gerarJson();
+function fabricarUsuario(){
+    let obj
+    let validador = true;
+    let name = document.getElementById('txtNome').value;
+    let email = document.getElementById('txtEmail').value;
+    let senha = document.getElementById('txtSenha').value;
+    let senhaConf = document.getElementById('txtSenhaConf').value;
+    let = cpf = document.getElementById('txtCPF').value
+    let = telefone = document.getElementById('txtTelefone').value
+    let dtNasc = document.getElementById('txtDtNascimento').value
+    
+    let usuario = fabricaDeUsuario(name, senha, senhaConf, email, cpf, telefone, dtNasc)
 
     
+    if(!usuario.validarEmail()) {validador = false}
+    if(!usuario.validarSenhas()) {validador = false}
+    if(!usuario.validarCPF()) {validador = false}
+
+
+    if(validador === true){
+        obj = usuario.gerarObjeto()
+        let data = JSON.stringify(obj)
+        usuario.postUsuario('http://localhost:8080/ControleDeLanche/register', obj)
+    }
+    debugger
+    console.log(obj)
+
+    
+
 }
+
+
 
