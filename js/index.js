3a8cb667fa76cd66df20911d7c2a56f347eaa935
Guilherme Rosa <swingthehorizon@hotmail.com>
2020-10-20 19:37:29 -0300

    aaa

--- js/index.js
+++ js/index.js
@@ -9,6 +9,14 @@ function login(){
     let senha = document.getElementById('txtSenha').value
 
     //Conectar com backend e trazer valores para validar se o login existe realmente
+    const url = "url";
+    fetch(url, { 
+        method: 'get' // opcional 
+      })
+      .then(function(response) { 
+        // use a resposta 
+      })
+      .catch(function(err) { console.error(err); });
         
     
 }
